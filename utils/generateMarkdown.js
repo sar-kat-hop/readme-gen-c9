65f66818e02df987fc0262978c0fe643996b7df1
Sarah <sarah.k.hopper@gmail.com>
2023-04-06 15:22:42 -0700

    refactor to use generateMarkdown after all

--- utils/generateMarkdown.js
+++ utils/generateMarkdown.js
@@ -1,20 +1,68 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+function renderLicenseBadge(license) {
+  if (license === 'MIT') {
+      return '[![License: MIT](https://img.shields.io/badge/License-MIT-yellow.svg)](https://opensource.org/licenses/MIT)';
+
+  } else if (license === 'Apache 2.0') {
+      return '[![License](https://img.shields.io/badge/License-Apache%202.0-blue.svg)](https://opensource.org/licenses/Apache-2.0)';
+
+  }  else if (license === 'GPL 3.0') {
+      return '[![License: GPL v3](https://img.shields.io/badge/License-GPLv3-blue.svg)](https://www.gnu.org/licenses/gpl-3.0)';
+
+  } else {
+      return '';
+  }
+};
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+      return 'https://opensource.org/licenses/MIT';
+
+  } else if (license === 'Apache 2.0') {
+      return 'https://opensource.org/licenses/Apache-2.0';
+
+  } else if (license === 'GPL 3.0') {
+      return 'https://www.gnu.org/licenses/gpl-3.0';
+
+  } else {
+    return '';
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  if (license === 'MIT' || 'Apache 2.0' || 'GPL 3.0') {
+    return `## License
+    [${answers.license}]
+    (${renderLicenseBadge(answers.license)})  
+    (${renderLicenseLink(answers.license)})
+    `;
+
+  } else {
+    return '';
+  };
+};
 
 // TODO: Create a function to generate markdown for README
-function generateMarkdown(data) {
-  return `# ${data.title}
+function generateMarkdown(answers) {
+  return `# ${answers.title}
+
+  ## About
+  ${answers.about}
+
+  ## Installation
+  ${answers.installation}
+
+  ## Contributions
+  ${answers.contributions}
+
+  ${renderLicenseSection(answers)} 
 
 `;
-}
+};
 
 module.exports = generateMarkdown;
